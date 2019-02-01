0e265bc8e25d4f6930f8e8f09613995ab611a152
Jenny Chu <jchudesign0930@gmail.com>
2019-02-01 04:35:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,5 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+describe "#introduction" do 
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
+    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+  end
 end
